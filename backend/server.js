377f0ec2b48306ae9cccea44752954b06b5c5e41
prathmesh0125 <bidveprathmesh590@gmail.com>
2023-08-17 20:37:30 +0530

    Creating server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api",(req,res)=>{
-    res.send("chats data");
+app.get("/api/chats",(req,res)=>{
+    res.send(chats);
+    // console.log("data");
+
+})
+app.get("/api/chat/:id",(req,res)=>{
+    // console.log(req.params.id)
+    const singelchat=chats.find((c)=>c._id===req.params.id);
+    res.send(singelchat)
 })
 
-app.listen(3000, console.log("server started on 3000 port"));
+const PORT=process.env.PORT || 5000
+app.listen(PORT, console.log(`server started on ${PORT} port`));
