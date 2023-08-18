d3567da14e0756a116b73fcc5710cd2310f1cf7d
prathmesh0125 <bidveprathmesh590@gmail.com>
2023-08-18 15:46:28 +0530

    create component

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Button  } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+  <Button colorScheme='blue'>Button</Button> 
+    </div>
+  );
+}
+
+export default App;
